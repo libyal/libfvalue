7f22babebcf28064b2bc65fc33b95bf07774218e
Joachim Metz <joachim.metz@gmail.com>
2016-08-31 06:46:18 +0200

    Worked on tests.

--- tests/fvalue_test_get_version.c
+++ tests/fvalue_test_get_version.c
@@ -20,7 +20,6 @@
  */
 
 #include <common.h>
-#include <file_stream.h>
 
 #if defined( HAVE_STDLIB_H ) || defined( WINAPI )
 #include <stdlib.h>
@@ -28,6 +27,7 @@
 
 #include "fvalue_test_libcstring.h"
 #include "fvalue_test_libfvalue.h"
+#include "fvalue_test_macros.h"
 #include "fvalue_test_unused.h"
 
 /* Tests retrieving the library version
@@ -46,11 +46,15 @@ int fvalue_test_get_version(
 	          LIBFVALUE_VERSION_STRING,
 	          9 );
 
-	if( result != 0 )
-	{
-		return( 0 );
-	}
+	FVALUE_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 0 );
+
 	return( 1 );
+
+on_error:
+	return( 0 );
 }
 
 /* The main program
@@ -68,10 +72,13 @@ int main(
 	FVALUE_TEST_UNREFERENCED_PARAMETER( argc )
 	FVALUE_TEST_UNREFERENCED_PARAMETER( argv )
 
-	if( fvalue_test_get_version() != 1 )
-	{
-		return( EXIT_FAILURE );
-	}
+	FVALUE_TEST_RUN(
+	 "libfvalue_get_version",
+	 fvalue_test_get_version() )
+
 	return( EXIT_SUCCESS );
+
+on_error:
+	return( EXIT_FAILURE );
 }
 
