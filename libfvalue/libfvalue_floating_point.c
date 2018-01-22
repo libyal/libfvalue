23128437c35d18f9bbf58d4f9cfab9e630d07aa7
Joachim Metz <joachim.metz@gmail.com>
2018-01-22 07:23:50 +0100

    Applied updates

--- libfvalue/libfvalue_floating_point.c
+++ libfvalue/libfvalue_floating_point.c
@@ -1,7 +1,7 @@
 /*
  * Floating point value (IEEE 754) functions
  *
- * Copyright (C) 2010-2017, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2010-2018, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
