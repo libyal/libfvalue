a4b6042dc138d5229cc28b394cdefef422c919bb
Joachim Metz <joachim.metz@gmail.com>
2019-01-03 18:32:31 +0100

    Corrected typos

--- libfvalue/libfvalue_utf8_string.c
+++ libfvalue/libfvalue_utf8_string.c
@@ -154,7 +154,7 @@ int libfvalue_utf8_string_split(
 		 error,
 		 LIBCERROR_ERROR_DOMAIN_RUNTIME,
 		 LIBCERROR_RUNTIME_ERROR_INITIALIZE_FAILED,
-		 "%s: unable to intialize split string.",
+		 "%s: unable to initialize split string.",
 		 function );
 
 		goto on_error;
