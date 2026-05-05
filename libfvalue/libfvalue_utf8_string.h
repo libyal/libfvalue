145e6e0fa72269ecc262babde4d7965559a89e20
Joachim Metz <joachim.metz@gmail.com>
2026-05-05 08:02:32 +0200

    Applied updates

--- libfvalue/libfvalue_utf8_string.h
+++ libfvalue/libfvalue_utf8_string.h
@@ -1,7 +1,7 @@
 /*
  * UTF-8 string value functions
  *
- * Copyright (C) 2010-2025, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2010-2026, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
